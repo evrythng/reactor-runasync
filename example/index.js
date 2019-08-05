1ef5dadf35ab77798b060743fd2b3a850453aa64
C-D-Lewis <Bonsitm@gmail.com>
2019-08-05 09:41:05 +0100

    Add example script

--- example/index.js
+++ example/index.js
@@ -1,25 +1,5 @@
-# reactor-runasync
+const runAsync = require('../');
 
-Helper function to enable easy use of async/await in Reactor scripts.
-
-
-## Setup
-
-Add to a Reactor script's dependency list, then use `require()`:
-
-```js
-const runAsync = require('reactor-runasync');
-```
-
-
-## Usage
-
-When a Reactor script uses a single promise chain, use `runAsync()` to run an
-`async` function that returns a Promise chain. The function will automatically
-handle catching and logging any errors thrown, and calling `done()` in all
-cases.
-
-```js
 /**
  * Read the action's Thng and update the tags to show it has been activated.
  */
@@ -38,4 +18,5 @@ const onActionCreatedAsync = async (event) => {
 
 // @filter(onActionCreated) action.type=_ItemProduced
 const onActionCreated = event => runAsync(() => onActionCreatedAsync(event));
-```
+
+module.exports = { onActionCreated };
